2ca8f3cf914ba3a0228a85d425080c97d17794ad
Joseph Collier <joejcollier.dev@gmail.com>
2024-03-22 09:34:10 -0400

    Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,39 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'azure': {
+                    300: '#F3F6F6',
+                    500: '#D1E0DF',
+                    700: '#A2C0BE',
+                    900: '#6D9A97'
+                },
+                'platinum': {
+                    300: '#F4F5F7',
+                    500: '#D6DBE4',
+                    700: '#ACB6C9',
+                    900: '#7B8AA7'
+                },
+                'floral-white': {
+                    300: '#FFFEFC',
+                    500: '#FFF9EE',
+                    700: '#FFF1D7',
+                    900: '#FAE1B1'
+                },
+                'seashell': {
+                    300: '#FFFDFC',
+                    500: '#FFF5EE',
+                    700: '#FFE8D7',
+                    900: '#FAD1B1'
+                }
+            }
         },
     },
+
+    purge: [
+        './resources/js/**/*.{vue,js}',
+        './resources/views/**/*.blade.php'
+    ],
 
     plugins: [forms],
 };
