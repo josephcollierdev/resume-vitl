8214841aa40b808dc1ea6f57495f88216177eb95
Joseph Collier <josephcollier.contact@gmail.com>
2024-04-02 06:03:34 -0400

    Styling

--- resources/js/Components/Contact.vue
+++ resources/js/Components/Contact.vue
@@ -39,17 +39,19 @@
 </script>
 
 <template>
-  <div class="min-h-lvh">
+  <div class="min-h-[86lvh]">
     <div id="section-contact" :class="sectionHeaderContainerClass">
       <div :class="sectionHeaderClass">CONTACT</div>
     </div>
 
-    <div :class="subContainerStylingClass" class="w-3/4 min-h-64 m-auto border-b-0">
+    <div :class="subContainerStylingClass" class="w-full px-[3.95rem] m-auto border-b-0">
       <div :class="subContainerContentStylingClass">
-        <div>Feel free to send any questions or inquiries to me here, via email:<br /><br /></div>
+        <div class="lg:mt-16">Feel free to send any questions or inquiries to me here, via email:<br />
+          <div class="hidden lg:block"><br /></div>
+        </div>
         <p><a class="md:hover:text-lg hover:tracking-widest transition-all duration-500 ease-in-out" href="mailto:josephcollier.contact@gmail.com">josephcollier.contact@gmail.com</a></p>
-        <div class="flex p-8 lg:p-16 m-auto w-full justify-center">
-          <div class="flex w-32 h-32 lg:w-64 lg:h-64 border" :class="verticalAlignClass">
+        <div class="flex p-0 lg:p-16 m-auto w-full justify-center">
+          <div class="flex w-32 h-32 lg:w-64 lg:h-64 border mt-4" :class="verticalAlignClass">
             <div class="text-base lg:text-4xl" :class="verticalAlignClass">JOSEPH COLLIER</div>
           </div>
         </div>
